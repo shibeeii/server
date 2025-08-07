671a14e26790890d0e9b8ed8d97bb670fdfbd641
shibeeii <shibilnalakath07@gmail.com>
2025-08-07 11:15:24 +0530

    logic change

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -3,26 +3,25 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, text, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail", // or use "hotmail", "yahoo", etc. or use host + port
+      service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER, // your Gmail address
-        pass: process.env.EMAIL_PASS, // app password
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     const mailOptions = {
-      from: `"Your Shop Name" <${process.env.EMAIL_USER}>`,
+      from: `"Q-Mart" <${process.env.EMAIL_USER}>`,
       to,
       subject,
-      text,
+      text: text || "Thanks for your order!",
       html,
     };
 
     const info = await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent: ", info.messageId);
+    console.log("✅ Email sent:", info.response);
   } catch (err) {
     console.error("❌ Error sending email:", err);
-    throw err;
   }
 };
 
